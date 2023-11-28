67e939e57685743385d408a4306c84fbb2872b03
Shyrynsky <shyrynsky@gmail.com>
2023-11-28 18:46:28 +0300

    добавил секцию MainTop

---
+++ src/components/MainTop/MainTop.jsx
@@ -0,0 +1,38 @@
+import { useTranslation } from "react-i18next";
+import { Link as Anchor } from "react-scroll";
+import './MainTop.css';
+import TopImage from '../../images/Man.png';
+import ArrowImage from '../../images/Arrow.svg';
+
+export function MainTop() {
+    const { t } = useTranslation("ns1");
+    return (
+        <section className="top_section">
+            <div className="container">
+                <div className="top_inner">
+                    <div className="top_info">
+                        <p className="top_paragraph_1">Person.by</p>
+                        <h1 className="top__title">{t("top-title")}</h1>
+                        <h2 className="top_paragraph_2">{t("top-paragraph")}</h2>
+                    </div>
+                    <div className="top_visual">
+                        <div className="top_y_circle"></div>
+                        <div className="top_w_circle"></div>
+                        <Anchor
+                            to="person_of_the_day_section"
+                            duration={500}
+                            smooth={true}
+                            className="top_scroll_Anchor"
+                        >
+                            <div className="top_button_circle">
+                                <p className="top_button_text">{t("top-button")}</p>
+                                <img src={ArrowImage} alt="arrow" />
+                            </div>
+                        </Anchor>
+                        <img src={TopImage} alt="top_image" className="top_image" />
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,9 +1,11 @@
 import { MainContent } from "../components/MainContent/MainContent.jsx";
 import { PersonOfTheDay } from "../components/PersonOfTheDay/PersonOfTheDay.jsx";
+import { MainTop } from "../components/MainTop/MainTop.jsx";
 
 export function MainPage() {
     return (
         <>
+            <MainTop />
             <MainContent />
             <PersonOfTheDay />
         </>
