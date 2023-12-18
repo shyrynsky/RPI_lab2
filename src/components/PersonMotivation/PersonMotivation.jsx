bcc5e0ea591fbab48601e94ce2009c8e5247283d
amy1181996 <amy1181996@gmail.com>
2023-12-19 00:21:07 +0300

    fixed hardcoded motivation title

--- src/components/PersonMotivation/PersonMotivation.jsx
+++ src/components/PersonMotivation/PersonMotivation.jsx
@@ -1,12 +1,15 @@
 import style from './PersonMotivation.module.css'
+import {useTranslation} from "react-i18next";
 
 export function PersonMotivation({person}) {
+    const { t } = useTranslation("ns1");
+
     return (
         <section className={style.motivation_section}>
             <div className='container'>
                 <div className={style.motivation_container}>
                     <div className={style.textblock}>
-                        <div className={style.motivation__title}>мотивация</div>
+                        <div className={style.motivation__title}>{t("motivation")}</div>
                         <div className={style.motivation__paragraph}>{person.motivation}</div>
                     </div>
 
